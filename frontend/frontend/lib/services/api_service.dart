d3252907bc765463904a80d793a40e302c4abf46
loickevin <lkseunkam@gmail.com>
2026-07-29 22:27:49 +0200

    Configure Flutter for railway API

--- frontend/frontend/lib/services/api_service.dart
+++ frontend/frontend/lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
 
-  static const String baseUrl = "http://127.0.0.1:8000";
+  static const String baseUrl = "https://helloworld-production-c38d.up.railway.app";
 
 
   static Future<String> getHello() async {
